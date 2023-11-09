299a9f2820b7b5956d1495f0a0f2befb06cf89b5
jehkke <jbndoit@gmail.com>
2023-11-09 15:41:49 +0900

    실습4

---
+++ 실습4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	char src[30] = "happy c programming";//[]안에 안넣어도 됨
+	
+	
+	
+	printf("string \"%s\" length   : %i\n", src, strlen(src));//문자열의 길이 
+	system("PAUSE");
+	return 0;
+}
