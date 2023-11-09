5220bd296b3b0d6d76bda912d2f4da8f6b90546c
jehkke <jbndoit@gmail.com>
2023-11-09 16:15:12 +0900

    실습6 미

---
+++ 실습6.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	FILE* fp;
+	char c;
+	
+	fp= fopen("sample.txt","r");
+	if (fp== NULL)
+	{
+		printf("failed to open\n");
+		return 0;
+	 } 
+	while ((c=fgetc(fp)) !=EOF)//한글자 읽은게 마지막이 아니면 
+	{
+		putchar(c);//한글자 출력 
+	}
+	fclose(fp);
+	
+	system("PAUSE");
+	return 0;
+}
+
+
+
+
+
+
+
+	 
+	
+	
+	
